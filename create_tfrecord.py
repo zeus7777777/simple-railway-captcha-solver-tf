eaeb2bd60d7f756935f54a9608e204b1fd5cc048
zeus7777777 <zeus840703@gmail.com>
2018-06-30 16:14:22 +0800

    fix tfrecord

--- create_tfrecord.py
+++ create_tfrecord.py
@@ -37,41 +37,23 @@ def main(captcha_file, prefix, tfrecord_path):
     all_lens = []
     all_captcha = []
 
-    '''captcha_files = [
-        #'data/5_imitate_train_set/captcha_train.csv'
-        #'data/5_imitate_vali_set/captcha_vali.csv'
-        #'data/6_imitate_train_set/captcha_train.csv'
-        #'data/6_imitate_vali_set/captcha_vali.csv'
-        'data/56_imitate_train_set/captcha_train.csv'
-    ]'''
-    captcha_files = ['data/56_imitate_vali_set/captcha_vali.csv']
-    '''prefix = [
-        #'data/5_imitate_train_set/',
-        #'data/5_imitate_vali_set/'
-        #'data/6_imitate_train_set/'
-        #'data/6_imitate_vali_set/'
-        'data/56_imitate_train_set/'
-    ]'''
-    prefix = ['data/56_imitate_vali_set/']
-
-    writer = tf.python_io.TFRecordWriter('tfrecord/images_valid_56.tfrecord')
+    writer = tf.python_io.TFRecordWriter(tfrecord_path)
     cnt = 0
 
-    for i in range(len(captcha_files)):
-        file_names, captchas = parse_csv_captcha(captcha_files[i])
-        for j in range(len(file_names)):
-            with tf.gfile.FastGFile(prefix[i]+file_names[j]+'.jpg' , 'rb') as f:
-                jpeg_str = f.read()
-            example = tf.train.Example(features=tf.train.Features(feature={
-                'jpeg_str': _bytes_feature(tf.compat.as_bytes(jpeg_str)),
-                'captcha_ints': _int64_feature(captcha_to_int(captchas[j])),
-            }))
-            writer.write(example.SerializeToString())
-            cnt += 1
-            if (cnt+1)%1000==0:
-                print(cnt+1, 'images processed')
-            if cnt<100:
-                print(file_names[j], captchas[j])
+    file_names, captchas = parse_csv_captcha(captcha_file)
+    for j in range(len(file_names)):
+        with tf.gfile.FastGFile(prefix[i]+file_names[j]+'.jpg' , 'rb') as f:
+            jpeg_str = f.read()
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'jpeg_str': _bytes_feature(tf.compat.as_bytes(jpeg_str)),
+            'captcha_ints': _int64_feature(captcha_to_int(captchas[j])),
+        }))
+        writer.write(example.SerializeToString())
+        cnt += 1
+        if (cnt+1)%1000==0:
+            print(cnt+1, 'images processed')
+        if cnt<100:
+            print(file_names[j], captchas[j])
     writer.close()
     print(cnt, 'images written')
 
